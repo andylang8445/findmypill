75b0528c555477efbff70235828682aae4045542
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2022-12-02 17:03:01 -0500

    .

--- API/cacher.py
+++ API/cacher.py
@@ -13,9 +13,9 @@ el_cache = list()
 def get_pill(din_code: str):
     """
     :param din_code: medication code
-    :return string data of the pill info:
+    :return dict data of the pill info:
     """
-    val: str = str()
+    val: dict = dict()
     if din_code in pill_cache_key:
         pill_idx: int = pill_cache_key.index(din_code)
         pill_access_counter[pill_idx] += 1
@@ -31,8 +31,36 @@ def get_pill(din_code: str):
         query_string = "SELECT * FROM %s WHERE DIN = %s;"
         query_data = (str(c.table['pill']), din_code)
         query_return = query_server(query_string, query_data)
+        return_li = list()
         for (name, element, id_code, din_code, company_name, dose_form, how_to_consume) in query_return:
             li = element.split(',')
+            sub_li: list = list()
+            sub_dict = dict()
+            for el_each in li:
+                ingredient_str: str = ''
+                amount: str = ''
+                ingredient_str, amount = el_each.split(':')
+                el_name_str = get_element(int(ingredient_str))
+                sub_li.append({el_name_str: str(amount) + "mg"})
+            sub_dict['ingredient'] = sub_li
+            if len(str(id_code)) == 0:
+                id_code = 'N/A'
+            if len(din_code) == 0:
+                din_code = 'N/A'
+            if len(company_name) == 0:
+                company_name = 'N/A'
+            if len(dose_form) == 0:
+                dose_form = 'N/A'
+            if len(how_to_consume) == 0:
+                how_to_consume = 'N/A'
+            sub_dict['name'] = name
+            sub_dict['id_code'] = id_code
+            sub_dict['din_code'] = din_code
+            sub_dict['company_name'] = company_name
+            sub_dict['dose_form'] = dose_form
+            sub_dict['how_to_consume'] = how_to_consume
+            return_li.append(sub_dict)
+        return return_li[0]
 
     # search cache
     # search DB
@@ -40,11 +68,16 @@ def get_pill(din_code: str):
     # Return the value
 
 
-def get_element(el_code: int):
+def get_element(el_code: int) -> str:
     """
     :param el_code:  element code
     :return: name of the corresponding element
     """
+    query_string = "SELECT Ingredient FROM %s WHERE Code = %s;"
+    query_data = (str(c.table['ingredient']), el_code)
+    query_return = query_server(query_string, query_data)
+    tmp_ingredient = list(query_return)[0][0]
+    return str(tmp_ingredient)
     # search cache
     # search DB
     # Update cache based on the data
